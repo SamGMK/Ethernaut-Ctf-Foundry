c9eb9328639e0b97fe05c4bc2e4e410b8312e76f
SamaterPhi7 <samaterphi77@gmail..com>
2022-04-20 04:54:30 -0700

    first git commit

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,12 +1,3 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
 
-import "ds-test/test.sol";
 
-contract ContractTest is DSTest {
-    function setUp() public {}
 
-    function testExample() public {
-        assertTrue(true);
-    }
-}
